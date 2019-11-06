dd5b81c1106b0653f76451a2a451edfd48fe2afa
Anthony Nahas <anthony.na@hotmail.de>
2019-11-06 21:12:17 +0100

    feat(project): added angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule
   ],
   providers: [],
