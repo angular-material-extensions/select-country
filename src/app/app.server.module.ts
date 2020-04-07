8340714a8138ac90ab2487780c32f87f62bff4e7
Anthony Nahas <anthony.na@hotmail.de>
2020-04-07 20:16:37 +0200

    fix(lib): support of prerendering

--- src/app/app.server.module.ts
+++ src/app/app.server.module.ts
@@ -3,10 +3,12 @@ import { ServerModule } from '@angular/platform-server';
 
 import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
+import {FlexLayoutServerModule} from '@angular/flex-layout/server';
 
 @NgModule({
   imports: [
     AppModule,
+    FlexLayoutServerModule,
     ServerModule
 ],
   bootstrap: [AppComponent],
