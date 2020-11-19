e7f18511a46323bf70abfec61945eafcea941209
Anthony Nahas <anthony.na@hotmail.de>
2020-11-20 00:05:05 +0100

    fix(lib): upgraded angular to v11

--- projects/angular-material-extensions/select-country/src/lib/mat-select-country.component.spec.ts
+++ projects/angular-material-extensions/select-country/src/lib/mat-select-country.component.spec.ts
@@ -1,4 +1,4 @@
-import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import {MatSelectCountryComponent} from './mat-select-country.component';
 import {MatAutocompleteModule} from '@angular/material/autocomplete';
@@ -13,7 +13,7 @@ describe('SelectCountryComponent', () => {
   let fixture: ComponentFixture<MatSelectCountryComponent>;
   let inputElement: HTMLInputElement;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [MatSelectCountryComponent],
       imports: [
