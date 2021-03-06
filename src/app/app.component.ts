8ae00cec8c5cc03e70cc5cc2cf1ae200f54228e9
Koussay HAJ KACEM <koussayhajkacem@gmail.com>
2021-03-06 22:21:50 +0100

    updated examples

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Country } from '@angular-material-extensions/select-country';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+
+import { Country } from '@angular-material-extensions/select-country';
 import { Angulartics2GoogleAnalytics } from 'angulartics2/ga';
 
 @Component({
@@ -9,7 +10,7 @@ import { Angulartics2GoogleAnalytics } from 'angulartics2/ga';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  title = 'select-county';
+  title = 'select-country';
 
   countryFormControl = new FormControl();
   countryRequiredFormControl = new FormControl(null, [Validators.required]);
@@ -19,7 +20,8 @@ export class AppComponent implements OnInit {
     name: 'Deutschland',
     alpha2Code: 'DE',
     alpha3Code: 'DEU',
-    numericCode: '276'
+    numericCode: '276',
+    callingCode: '+49'
   };
 
   predefinedCountries: Country[] = [
@@ -27,25 +29,29 @@ export class AppComponent implements OnInit {
       name: 'Germany',
       alpha2Code: 'DE',
       alpha3Code: 'DEU',
-      numericCode: '276'
+      numericCode: '276',
+      callingCode: '+49'
     },
     {
       name: 'Greece',
       alpha2Code: 'GR',
       alpha3Code: 'GRC',
-      numericCode: '300'
+      numericCode: '300',
+      callingCode: '+30'
     },
     {
       name: 'France',
       alpha2Code: 'FR',
       alpha3Code: 'FRA',
-      numericCode: '250'
+      numericCode: '250',
+      callingCode: '+33'
     },
     {
       name: 'Belgium',
       alpha2Code: 'BE',
       alpha3Code: 'BEL',
-      numericCode: '056'
+      numericCode: '056',
+      callingCode: '+32'
     }
   ];
 
@@ -56,10 +62,6 @@ export class AppComponent implements OnInit {
     angulartics2GoogleAnalytics.startTracking();
   }
 
-  onCountrySelected($event: Country) {
-    console.log($event);
-  }
-
   ngOnInit(): void {
     this.countryFormGroup = this.formBuilder.group({
       country: [
@@ -68,7 +70,8 @@ export class AppComponent implements OnInit {
             name: 'Deutschland',
             alpha2Code: 'DE',
             alpha3Code: 'DEU',
-            numericCode: '276'
+            numericCode: '276',
+            callingCode: '+49'
           },
           disabled: false
         }
@@ -87,5 +90,9 @@ export class AppComponent implements OnInit {
     this.countryFormControl.valueChanges.subscribe((country) =>
       console.log('this.countryFormControl.valueChanges', country)
     );
+  }
+
+  onCountrySelected($event: Country): void {
+    console.log($event);
   }
 }
