4c26efee5e68a8ee6da2ceb50a4104ac8a1314e7
Anthony Nahas <anthony.na@hotmail.de>
2022-11-30 21:05:42 +0100

    fix(lib): adapt lib to v15

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
+import { MatSelectModule } from '@angular/material/select';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
-import { MatLegacySelectModule as MatSelectModule } from '@angular/material/legacy-select';
 import { MatSelectCountryModule } from '@angular-material-extensions/select-country';
 import { MarkdownModule } from 'ngx-markdown';
 import { Angulartics2Module } from 'angulartics2';
