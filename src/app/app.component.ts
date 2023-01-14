20ecad481c1f4d5a25c54674943e291bd8e5d1e3
Roger De Moreta <rogermoreta@gmail.com>
2023-01-14 02:44:10 +0100

    #87  #75

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,61 +1,72 @@
-import { Component, OnInit } from '@angular/core';
-import { UntypedFormBuilder, UntypedFormControl, UntypedFormGroup, Validators } from '@angular/forms';
+import { Component, OnInit } from "@angular/core";
+import {
+  FormControl,
+  FormGroup,
+  UntypedFormBuilder,
+  UntypedFormControl,
+  UntypedFormGroup,
+  Validators,
+} from "@angular/forms";
 
-import { Country } from '@angular-material-extensions/select-country';
-import { Angulartics2GoogleAnalytics } from 'angulartics2/ga';
+import { Country } from "@angular-material-extensions/select-country";
+import { Angulartics2GoogleAnalytics } from "angulartics2/ga";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent implements OnInit {
-  title = 'select-country';
+  title = "select-country";
 
   countryFormControl = new UntypedFormControl();
-  countryRequiredFormControl = new UntypedFormControl(null, [Validators.required]);
+  countryRequiredFormControl = new UntypedFormControl(null, [
+    Validators.required,
+  ]);
   countryFormGroup: UntypedFormGroup;
 
-  languageSelected = 'es';
-  showCallingCode = 'false';
+  languageSelected = "es";
+  showCallingCode = "false";
 
   defaultValue: Country = {
-    name: 'Deutschland',
-    alpha2Code: 'DE',
-    alpha3Code: 'DEU',
-    numericCode: '276',
-    callingCode: '+49'
+    name: "Deutschland",
+    alpha2Code: "DE",
+    alpha3Code: "DEU",
+    numericCode: "276",
+    callingCode: "+49",
   };
+
+  countryMandatoryFormControl = new FormControl(null, [Validators.required]);
 
   predefinedCountries: Country[] = [
     {
-      name: 'Germany',
-      alpha2Code: 'DE',
-      alpha3Code: 'DEU',
-      numericCode: '276',
-      callingCode: '+49'
+      name: "Germany",
+      alpha2Code: "DE",
+      alpha3Code: "DEU",
+      numericCode: "276",
+      callingCode: "+49",
     },
     {
-      name: 'Greece',
-      alpha2Code: 'GR',
-      alpha3Code: 'GRC',
-      numericCode: '300',
-      callingCode: '+30'
+      name: "Greece",
+      alpha2Code: "GR",
+      alpha3Code: "GRC",
+      numericCode: "300",
+      callingCode: "+30",
     },
     {
-      name: 'France',
-      alpha2Code: 'FR',
-      alpha3Code: 'FRA',
-      numericCode: '250',
-      callingCode: '+33'
+      name: "France",
+      alpha2Code: "FR",
+      alpha3Code: "FRA",
+      numericCode: "250",
+      callingCode: "+33",
     },
     {
-      name: 'Belgium',
-      alpha2Code: 'BE',
-      alpha3Code: 'BEL',
-      numericCode: '056',
-      callingCode: '+32'
-    }
+      name: "Belgium",
+      alpha2Code: "BE",
+      alpha3Code: "BEL",
+      numericCode: "056",
+      callingCode: "+32",
+    },
   ];
 
   constructor(
@@ -70,32 +81,39 @@ export class AppComponent implements OnInit {
       country: [
         {
           value: {
-            name: 'Deutschland',
-            alpha2Code: 'DE',
-            alpha3Code: 'DEU',
-            numericCode: '276',
-            callingCode: '+49'
+            name: "Deutschland",
+            alpha2Code: "DE",
+            alpha3Code: "DEU",
+            numericCode: "276",
+            callingCode: "+49",
           },
-          disabled: false
-        }
-      ]
+          disabled: false,
+        },
+      ],
+    });
+
+    this.countryMandatoryFormControl.valueChanges.subscribe((country) => {
+      console.log(
+        "this.countryMandatoryFormControl.get('country').valueChanges",
+        country
+      );
     });
 
     this.countryFormGroup
-      .get('country')
+      .get("country")
       .valueChanges.subscribe((country) =>
-      console.log(
-        'this.countryFormGroup.get(\'country\').valueChanges',
-        country
-      )
-    );
+        console.log(
+          "this.countryFormGroup.get('country').valueChanges",
+          country
+        )
+      );
 
     this.countryFormControl.valueChanges.subscribe((country) =>
-      console.log('this.countryFormControl.valueChanges', country)
+      console.log("this.countryFormControl.valueChanges", country)
     );
   }
 
   onCountrySelected($event: Country): void {
-    console.log($event);
+    console.log("onCountrySelected", $event);
   }
 }
