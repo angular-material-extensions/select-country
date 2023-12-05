0f582cc71bf47f4a55b0b8dee3961242a6079a6f
Johan Lipecki <lipecki@kth.se>
2023-12-05 21:36:30 +0100

    Update mat-select-country.component.ts with Swedish case for language import

--- projects/angular-material-extensions/select-country/src/lib/mat-select-country.component.ts
+++ projects/angular-material-extensions/select-country/src/lib/mat-select-country.component.ts
@@ -485,6 +485,10 @@ export class MatSelectCountryComponent
         return import("./i18n/ru")
           .then((result) => result.COUNTRIES_DB_RU)
           .then((y) => y);
+      case "se":
+        return import("./i18n/se")
+          .then((result) => result.COUNTRIES_DB_SE)
+          .then((y) => y);
       case "uk":
         return import("./i18n/uk")
           .then((result) => result.COUNTRIES_DB_UA)
